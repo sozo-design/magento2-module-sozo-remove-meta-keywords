b25c26c1c8f577a9ae92155beaa2fa1644d333a2
Clive Walkden <clive@sozodesign.co.uk>
2023-06-22 07:19:21 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Sozo_RemoveMetaKeywords module
+
+# Sozo_RemoveMetaKeywords module
+The Sozo_RemoveMetaKeywords module removes the redundant `<meta name="keywords">` tag from the head section of your Magento website.
+
+This module should work with other modules that manipulate Magento for SEO purposes. The module has only been tested with Mirasvit SEO module so far.
+
+## Installation details
+
+To install the module on your Magento site follow these steps:
+1. Using Composer, run `composer require sozo/module-remove-meta-keywords`
+2. Run `bin/magento setup:upgrade`
+3. Run `bin/magento setup:di:compile`
+4. Run `bin/magento setup:static-content:deploy`
+5. Run `bin/magento cache:flush`
+
---
+++ registration.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ *  SOZO Design Ltd
+ *
+ *  NOTICE OF LICENSE
+ *
+ *  This source file is subject to the Open Software License (OSL 3.0)
+ *  that is bundled with this package in the file LICENSE.
+ *  It is also available through the world-wide-web at this URL:
+ *  http://opensource.org/licenses/osl-3.0.php
+ *
+ *  @category    SOZO Design Ltd
+ *  @package     Sozo_RemoveMetaKeywords
+ *  @copyright   Copyright (c) SOZO Design Ltd (https://sozodesign.co.uk)
+ *  @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Sozo_RemoveMetaKeywords', __DIR__);
